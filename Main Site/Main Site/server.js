ea0bc318d83467703949f82fab648afbb2c3e137
pspchucky <sergiogermanjr@csu.fullerton.edu>
2015-08-12 01:32:42 -0700

    Added new and improved HTML design, using Chartist instead, and using online DB service 'orchestrate.io'

--- Main Site/Main Site/server.js
+++ Main Site/Main Site/server.js
@@ -1,6 +1,26 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Coming soon\n');
-}).listen(port);
+var express = require('express');
+var oio = require('orchestrate');
+var token = 'Your-Token-Here';
+var db = oio(token, 'api.ctl-uc1-a.orchestrate.io');
+console.log("Connecting to orchestrate.io...");
+db.ping()
+.then(function () {
+    console.log("Valid orchestrate.io key detected.");
+    runWebServer();
+})
+.fail(function (err) {
+    console.log('Invalid orchestrate.io API key! Check your token => ' + token);
+    process.exit(1);
+});
+
+function runWebServer(){
+    var app = express();
+    app.use(express.static(__dirname + '/www'));
+    app.get('/', function(req, res) {
+        res.sendFile('www/index.html', {root: __dirname })
+    });
+    var port = process.env.port || 1337;
+    var server = app.listen(port);
+    console.log("Listening on port " + port);
+};
